d56458456b6becdf30fb08c60d4989c82b0602c8
k16051ho <49297400+k16051ho@users.noreply.github.com>
2019-06-28 10:28:03 +0900

    Update server.c

--- www/server.c
+++ www/server.c
@@ -14,14 +14,43 @@ void DieWithError(char *errorMessage){
 void commun(int sock){
     char buf[BUF_SIZE];
     int len_r;
-
-    if((len_r=recv(sock,buf,BUF_SIZE,0))<=0)
+	
+	
+	/*while文中内容何をやっているか理解しておくこと。TEST！！！！！*/
+	while((len_r=recv(sock,buf,BUF_SIZE,0))>0)/*空行が現れるまで*/)
+	{
+		buf[len_r] = '\0';
+		
+		printf("%s\n,buf");
+		
+		if (strstr(buf,"\r\n\r\n")){
+			printf("received HTTP request.");
+		break;
+		}
+	}
+	
+	if("len_r <= 0")
+		DieWithError("received() failed.");
+		printf("received HTTP Request./n");
+		
+	sprintf(response,"HTTP/1.1 200 OK\r\n");
+if(send(sock,responce,strlen(response,0)!=s))
+DieWithError("send() sent a message of unexpe)
+	
+/*
+    	if((len_r=recv(sock,buf,BUF_SIZE,0))<=0)
         DieWithError("recv()failed");
     
     buf[len_r] = '\0';
     printf("%s\n",buf);
+	}
+*/
+
+	/*↑メッセージの受信*/
+	
     if((send(sock,buf,strlen(buf),0))!=strlen(buf))
         DieWithError("send()sent a message of unexpected bytes");
+	/*↑メッセージの送信*/
 }
 
 int main(int argc, char **argv) {
